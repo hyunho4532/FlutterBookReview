2ee834d6bd97bf565d8874a2072fe5c315c3bcaf
hyunho4532 <kyounguk192@gmail.com>
2023-11-24 22:05:18 +0900

    2023-11-24 로그인 repository 만들기

---
+++ lib/src/common/repository/authentication_repository.dart
@@ -0,0 +1,29 @@
+import 'package:book_review/src/common/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthenticationRepository {
+  final FirebaseAuth _firebaseAuth;
+
+  AuthenticationRepository(this._firebaseAuth);
+
+  Stream<UserModel?> get user {
+    return _firebaseAuth.authStateChanges().map<UserModel?>((user) {
+      return user == null ? null : UserModel(name: user.displayName, uid: user.uid, email: user.email);
+    });
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
